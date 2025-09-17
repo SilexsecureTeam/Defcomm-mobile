b6caa3af2c7d900343fbf6e987335d256093662b
Ibraheem Ameen <ibraheemameen941@gmail.com>
2025-09-17 11:07:44 +0100

    completed the otp screen

--- lib/presentation/otp/screen/verify_otp.dart
+++ lib/presentation/otp/screen/verify_otp.dart
@@ -1,5 +1,7 @@
 import 'package:defcomm_mobile/core/theme/app_colors.dart';
+import 'package:defcomm_mobile/core/theme/app_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class VerifyOtpScreen extends StatefulWidget {
   const VerifyOtpScreen({super.key});
@@ -9,6 +11,7 @@ class VerifyOtpScreen extends StatefulWidget {
 }
 
 class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
+  String otpCode = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +42,72 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
               ),
             ),
 
-            Text('Verify OTP'),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Verify OTP',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: AppFonts.defaultFont,
+                        ),
+                      ),
+                      Text(
+                        'Use PIN to unlock fast and',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: AppFonts.defaultFont,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        'secure way',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: AppFonts.defaultFont,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 30),
+
+                  SizedBox(
+                    width: 250,
+                    child: PinCodeTextField(
+                      onCompleted: (value) {},
+                      appContext: context,
+                      length: 4,
+                      onChanged: (value) {
+                        setState(() {
+                          otpCode = value;
+                        });
+                      },
+                      keyboardType: TextInputType.number,
+                      pinTheme: PinTheme(
+                        activeFillColor: AppColors.primaryColor,
+                        borderWidth: 1,
+                        shape: PinCodeFieldShape.box,
+                        borderRadius: BorderRadius.circular(15),
+                        fieldHeight: 59.67,
+                        fieldWidth: 52.21,
+                        activeColor: Colors.grey,
+                        selectedColor: AppColors.primaryColor,
+                        inactiveColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
